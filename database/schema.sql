07c41945124eeaa7a8b4b2d5b40bd1a5c7764a75
sanjanasnvs <sanjanasrini30@gmail.com>
2025-05-19 11:28:36 +0100

    Updated password_hash to password
    For compatibility with Django expectations

--- database/schema.sql
+++ database/schema.sql
@@ -12,7 +12,7 @@ USE CVD_Risk;
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     role ENUM('patient', 'clinician_pending', 'clinician_approved') NOT NULL
